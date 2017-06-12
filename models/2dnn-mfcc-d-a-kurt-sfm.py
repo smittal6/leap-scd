8b3c530ea62115bc30f2450e110686fcececb629
Siddharth Mittal <smittal6@gmail.com>
2017-06-12 12:23:08 +0530

    Convert to categorical, use softmax

--- models/2dnn-mfcc-d-a-kurt-sfm.py
+++ models/2dnn-mfcc-d-a-kurt-sfm.py
@@ -31,7 +31,11 @@ valfile='/home/siddharthm/scd/combined/val-mfcc-kurt-sfm-labels.htk'
 
 def filter_data(x):
         ### Filter the data. That is only keep 0 or 1 classes.
-        return x[(x[:,-1]==0)|(x[:,-1]==1)]
+        indexes0=np.where(x[:,-1]==0)[0]
+        indexes1=np.where(x[:,-1]==1)[0]
+        temp=np.hstack((indexes0,indexes1))
+        tempsort=np.sort(temp)
+        return x[temp]
 
 def load_data_train(trainfile):
         print "Getting the overlap training data"
@@ -40,7 +44,12 @@ def load_data_train(trainfile):
         print "Done with Loading the training data: ",train_data.shape
         data=filter_data(train_data)
         x_train=train_data[:,:-1] #Set to different column based on different model
-        y_train=train_data[:,-1]
+        Y_train=train_data[:,-1]
+        print Y_train.shape
+        print np.where(Y_train==2)
+        Y_train=Y_train.reshape(Y_train.shape[0],1)
+        Y_train=Y_train.astype(np.int8)
+        y_train=np_utils.to_categorical(Y_train,2)
         del data
         return x_train,y_train
 
@@ -50,7 +59,10 @@ def load_data_test(testfile):
         print "Done loading the testing data: ",data.shape
         data=filter_data(data)
         x_test=data[:,:-1]
-        y_test=data[:,-1]
+        Y_test=data[:,-1]
+        print np.where(Y_test==2)
+        Y_test=np.reshape(Y_test,(Y_test.shape[0],1))
+        y_test=np_utils.to_categorical(Y_test,2)
         del data
         return x_test,y_test
 
@@ -60,7 +72,9 @@ def load_data_val(valfile):
         print "Done loading the validation data: ",data.shape
         data=filter_data(data)
         x_val=data[:,:-1]
-        y_val=data[:,-1]
+        Y_val=data[:,-1]
+        Y_val=np.reshape(Y_val,(Y_val.shape[0],1))
+        y_val=np_utils.to_categorical(Y_val,2)
         del data
         return x_val,y_val
 
@@ -72,14 +86,14 @@ def seq(x_train,y_train,x_val,y_val,x_test,y_test):
         model.add(Dense(256,activation='relu',input_dim=(41))) #Hidden layer1
         model.add(Dense(256,activation='relu')) #Hidden layer 2
         model.add(Dropout(0.5))
-        model.add(Dense(1,activation='sigmoid')) #Output Layer
+        model.add(Dense(2,activation='softmax')) #Output Layer
         #Compilation region: Define optimizer, cost function, and the metric?
         model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
         #Fitting region:Get to fit the model, with training data
         checkpointer=ModelCheckpoint(filepath=direc+common_save+'.json',monitor='val_acc',save_best_only=True,save_weights_only=True)
 
         #Doing the training[fitting]
-        model.fit(x_train,y_train,nb_epoch=10,batch_size=batch,validation_data=(x_val,y_val),callbacks=[checkpointer])
+        model.fit(x_train,y_train,epochs=10,batch_size=batch,validation_data=(x_val,y_val),callbacks=[checkpointer])
         model.save_weights(direc+common_save+'-weights'+'.json') #Saving the weights from the model
         model.save(direc+common_save+'-model'+'.json')#Saving the model as is in its state
 
@@ -92,15 +106,16 @@ def seq(x_train,y_train,x_val,y_val,x_test,y_test):
         scores_test=model.predict(x_test,batch_size=batch)
         sio.savemat(direc+name_test+'.mat',{'scores':scores_test,'ytest':y_test})
         ### ------------- ###
-
+        print model.evaluate(x_test,y_test,batch_size=batch)
         ### For finding the details of classification ###
-        #correct_overlap=0
-        """for i in range(len(x_test)):
+        correct_change=0
+        predictions=model.predict(x_test,batch_size=batch)
+        for i in range(len(x_test)):
                 if y_test[i]==1 and predictions[i]==1:
-                        correct_overlap+=1
-        print correct_overlap"""
+                        correct_change+=1
+        print "Correct changes detected: ",correct_change
+        print "Total speaker change frames: ",len(y_test[y_test==1])
         ### ------------- ###
-
 #Non-function section
 x_train,y_train=load_data_train(trainfile)
 print "Loading training data complete"
