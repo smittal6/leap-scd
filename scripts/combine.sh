0c6805e185495c09a7168db0c1fb79827b3fe2c5
Siddharth Mittal <smittal6@gmail.com>
2017-06-15 15:21:30 +0530

    modify script for gamma feat gen

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -1,10 +1,16 @@
-for i in $(seq 0 9); do
-        python combine_train.py x0$i $i &
+# Important: Right now the configuration for file is set for generating the gamma features combined
+
+#for i in $(seq 0 9); do
+        #python combine_train.py x0$i $i &
         #echo $i
-done
+#done
 
-for i in $(seq 0 4); do
-        python combine_test.py x0$i $i &
-        python combine_val.py x0$i $i &
-done
+#for i in $(seq 0 4); do
+        #python combine_test.py x0$i $i &
+        #python combine_val.py x0$i $i &
+#done
 
+#The combine_gamma program takes as input 3 arguments
+python combine_gamma.py train/ trainfile.list train
+python combine_gamma.py test/ testfile.list test
+python combine_gamma.py val/ valfile.list val
