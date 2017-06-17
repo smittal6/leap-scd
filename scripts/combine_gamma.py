625cdfc305fd7b77d6ba9364ec2e1f6583058d80
Siddharth Mittal <smittal6@gmail.com>
2017-06-17 18:02:07 +0530

    save number of lables in external file

--- scripts/combine_gamma.py
+++ scripts/combine_gamma.py
@@ -54,31 +54,35 @@ def data_creator(num,addr,file_reader,filename):
 
                 ### Kurtosis and sfm are row vectors, that is (1,Number of frames)
                 ### GAMMATONE -- LABEL   <--- Structure of the final matrix
-                # try:
-                read_data=data_read.getall()
-                read_data=filter_data(read_data)
-                scdlab+=len(np.where(read_data[:,-1]==1)[0])
-                noscdlab+=read_data.shape[0]-len(np.where(read_data[:,-1]==1)[0])
+                try:
+                        read_data=data_read.getall()
+                        read_data=filter_data(read_data)
+                        scdlab+=len(np.where(read_data[:,-1]==1)[0])
+                        noscdlab+=read_data.shape[0]-len(np.where(read_data[:,-1]==1)[0])
                         #id1 and id2 are integers essentially. if male then 1, if female than 0
-                id1=(0,1)[(file_reader[i][0]=='M')==True]
-                temp_index=file_reader[i].index("-")
-                id2=(0,1)[(file_reader[i][temp_index+1]=='M')==True]
-                gender_label=return_vec(read_data[:,-1],id1,id2)
+                        id1=(0,1)[(file_reader[i][0]=='M')==True]
+                        temp_index=file_reader[i].index("-")
+                        id2=(0,1)[(file_reader[i][temp_index+1]=='M')==True]
+                        gender_label=return_vec(read_data[:,-1],id1,id2)
                         # kurt_vector=np.transpose(kurt_matrix)
                         # sfm_vector=np.transpose(sfm_matrix)
                         # label_vector=np.transpose(labels_this_file)
                         # final_vector=np.hstack((read_data,kurt_vector,sfm_vector,label_vector))
-                final_vector=np.hstack((read_data,gender_label))
+                        final_vector=np.hstack((read_data,gender_label))
                         # matrix=np.vstack((matrix,final_vector))
-                del read_data
-                # except:
-                        # print "In the corrupt file section"
-                        # corrupt_files+=1
-                        # continue
+                        del read_data
+                except:
+                        print "In the corrupt file section"
+                        corrupt_files+=1
+                        continue
                         # ind=ind+read_data.shape[0]
                 #HTK supports concatenation, so we don't have to deal with numpy matrix again and again
                 writer.writeall(final_vector)
         print('corrput_files',corrupt_files)
+        f=open(save_extra,'w')
+        write_string=str(scdlab)+","+str(noscdlab)
+        f.write(write_string)
+        f.close()
 
 # First sys input is whether test/, train/ or val/ and second input is trainfile.list or ...., third is train, test or val
 addr='/home/siddharthm/scd/context/'+str(sys.argv[1])#address of the HTK files stored somewhere
@@ -88,6 +92,7 @@ addr='/home/siddharthm/scd/context/'+str(sys.argv[1])#address of the HTK files s
 num=40*64+1+1 #The length of the feature vector, to be read and stored in the htk format[Right now,40*64 Gammatone+1 Label+1 gender]
 file_read='/home/siddharthm/scd/lists/'+str(sys.argv[2]) #The raw filenames, in the form of list
 filename='gamma-labels-gender-'+str(sys.argv[3]) #The name of the file where stuff is going to be stored
+save_extra='extra-'+str(sys.argv[3])+'.txt' #For saving the count of lables
 file_reader=file_opener(file_read) #Calling the function to read the list of files
 # file_reader=['FAJW0_I1263-FCYL0_X349-9346','FAJW0_I1263-FGCS0_X226-13892']
 data_creator(num,addr,file_reader,filename) #Finally call the data creator
