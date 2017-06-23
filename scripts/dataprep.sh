2a677788bc4ffaedfdf902404013b6466f607e5b
Siddharth Mittal <smittal6@gmail.com>
2017-06-23 11:26:01 +0530

    update address for htk toolkit

--- scripts/dataprep.sh
+++ scripts/dataprep.sh
@@ -1,4 +1,4 @@
-HTKDIR="/home/neerajs/Desktop/htk/HTKTools"
+HTKDIR="/state/partition1/softwares/HTKTools"
 
 ## Takes as input the list of files, where column 1 is the list of wav files, and the second column is the list of files where the features are going to be stored
 
